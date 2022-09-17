8a5c6aa3bff984955d0d24557ec74db914986634
Bea Oliveira <beatriz@kramphub.com>
2022-09-17 16:04:31 -0300

    add code from September 17 lecture

--- readme.md
+++ readme.md
@@ -9,22 +9,3 @@
 * Every question is welcomed. Remember that your question might not be only yours. By asking your question, you help the group
 * To interact, you can either type on the chat or open your mic and talk
 * Do not compare your learning process with others. Rather, observe how today you know more than yesterday and create your own baseline
-
-----
-## Notes: 
-
-### Review on how the internet works + CRUD 
-You can check the slides about this subject [here](https://docs.google.com/presentation/d/1O74NbLfeYdkgChSo-QEVH-6dmeRoQX4vvWSq4CeZOYQ/edit?usp=sharing).
-
-### Creating our Favorite Movies Web App
-
-1. Create an Express Server -> you can find more information on how to install it [here](https://expressjs.com/en/starter/installing.html)
-2. Add [nodemon](https://www.npmjs.com/package/nodemon) so your server will watch for your changes. Otherwise, each time you make a change on your server, you will need to re-start the server
-3. Add [ejs](https://ejs.co/#install) to allow you to create html templates
-4. Don't forget to add the [body-parser](https://www.npmjs.com/package/body-parser) so you actually access your request's body
-
-**Remember:**  
-
-* `<a>`tags only work for GET request
-* `<form>`tags can be used both for GET and POST requests
-* how to pass info from your server to ejs: template variables
--- server.js
+++ server.js
@@ -1,42 +1,65 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
-const port = 8000
-
-const favoriteMovies = [
-    {
-        id: 0,
-        title: "Nope!",
-        year: 2022,
-        entryBy: 'Bea'
-    },
-    {
-        id: 1, 
-        title: 'Everything everywhere all at once',
-        year: 2021,
-        entryBy: 'Bea'
-
-    }
-]
+const port = 8080
 
 app.set('view engine', 'ejs')
 app.use(bodyparser.urlencoded({extended: true}))
 
+const favoriteMovies = [
+    {
+        id: 0,
+        title: "Mulan" ,
+        year: 2012,
+        entryBy: "Bea"
+    }, 
+    {
+        id: 1,
+        title: "Fight Club",
+        year: 1999,
+        entryBy: "Macky"
+    },
+    {
+        id: 2,
+        title: "Inception",
+        year: 2010,
+        entryBy: "Jevaughn"
+    },
+    {
+        id:3,
+        title: "Blade Runner 2049",
+        year: 2017,
+        entryBy: "Vanessa"
+    }
+]
+
+//GET
 app.get('/', (req, res) => {
-    res.render('app', {favoriteMovies})
+   res.render('app', {movies: favoriteMovies})
 })
-
-app.get('/add', (req, res) => {
+app.get("/add", (req, res) => {
     res.render('add')
-
 })
-app.get("/edit/:id", (req, res) => {
-    res.render('edit',{movie: favoriteMovies[req.params.id]} )
+app.get("/edit/:id", (req, res)=> {
+    console.log('id', req.params.id)
+    res.render('edit', {movie: favoriteMovies[req.params.id]})
 })
 
-app.post("/edit/:id", (req, res) => {
-    const newMovie ={
-        id: req.params.id, 
+//POST
+app.post("/add", (req, res)=> {
+    const newMovie = {
+        id: favoriteMovies.length,
+        title: req.body.title,
+        year: req.body.year,
+        entryBy: req.body.entryBy
+    }
+    favoriteMovies.push(newMovie)
+    res.redirect('/')
+})
+
+app.post("/edit/:id", (req, res)=> {
+    const newMovie = {
+        id: req.params.id,
         title: req.body.title,
         year: req.body.year,
         entryBy: req.body.entryBy
@@ -44,22 +67,14 @@ app.post("/edit/:id", (req, res) => {
     favoriteMovies[req.params.id] = newMovie
     res.redirect('/')
 })
-app.post("/add", (req, res) => {
-    const newMovie ={
-        id: favoriteMovies.length - 1, 
-        title: req.body.title,
-        year: req.body.year,
-        entryBy: req.body.entryBy
-    }
-    console.log('movie id ', favoriteMovies.length)
-    favoriteMovies.push(newMovie)
-    res.redirect('/')
-})
-app.post("/delete/:id", (req, res) => {
-    favoriteMovies.splice(req.params.id - 1, 1)
-    res.redirect('/')
+
+app.post("/delete/:id", (request, response)=> {
+    const elementPosition = request.params.id
+    console.log('elPosition', elementPosition)
+    favoriteMovies.splice(elementPosition, 1)
+    response.redirect("/")
 })
 
 app.listen(port, () => {
-    console.log(`My app is running on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
